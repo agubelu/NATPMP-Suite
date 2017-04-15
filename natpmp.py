023a691739fed0b378065953c630a91137b786d8
agubelu <agustinbd3@gmail.com>
2017-04-15 17:12:03 +0200

    First, testing version of the network module

--- natpmp.py
+++ natpmp.py
@@ -1,7 +1,15 @@
-from config_module import process_command_line_params
+from config_module      import process_command_line_params
+from network_module     import initialize_network_sockets
+
 import time
 
 
 if __name__ == "__main__":
+    # Initialize the current time to populate the "seconds since boot" parameter for the responses
     DAEMON_START_TIME = time.time()
+
+    # Process the command-line parameters
     process_command_line_params()
+
+    # Start the UDP sockets to listen for requests from the clients
+    initialize_network_sockets()
---
+++ network_module.py
@@ -0,0 +1,26 @@
+from common_utils import printlog
+
+import socket
+import settings
+import select
+
+def initialize_network_sockets():
+    private_ips = settings.PRIVATE_INTERFACES
+    udp_port = 5351
+    sockets = []
+
+    for ip in private_ips:
+        # Iterate over the private interfaces to provide a UDP socket for each one
+        udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp_sock.bind((ip, udp_port))
+        sockets.append(udp_sock)
+
+        printlog("Creating socket on %s:%d" % (ip, udp_port))
+
+    # Infinite network loop to attend requests
+    while True:
+        # Blocking call that will wait until a socket becomes available with data
+        ready_sockets, _, _ = select.select(sockets, [], [])
+        for sock in ready_sockets:
+            data, address = sock.recvfrom(2048)
+            printlog("Received '%s' from %s" % (data.decode("utf-8"), address))
