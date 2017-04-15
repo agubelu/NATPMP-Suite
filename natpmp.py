cc5959a10d9668c80c2bb01e3f8ed39dd378bed8
agubelu <agustinbd3@gmail.com>
2017-04-15 17:56:33 +0200

    Set the start time constant at a module import level

--- natpmp.py
+++ natpmp.py
@@ -3,10 +3,10 @@ from network_module     import initialize_network_sockets
 
 import time
 
+# Initialize the current time to populate the "seconds since boot" parameter for the responses
+DAEMON_START_TIME = time.time()
 
 if __name__ == "__main__":
-    # Initialize the current time to populate the "seconds since boot" parameter for the responses
-    DAEMON_START_TIME = time.time()
 
     # Process the command-line parameters
     process_command_line_params()
