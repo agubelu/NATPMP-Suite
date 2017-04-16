10a2c061fbcea8ba72954157ba3cdaa2284ddc44
agubelu <agustinbd3@gmail.com>
2017-04-16 13:48:35 +0200

    Further testing into network module

--- natpmp_logic_common.py
+++ natpmp_logic_common.py
@@ -6,12 +6,34 @@ import time
 
 def process_received_packet(data, address, sock):
 
-    data_str = data.decode("utf-8")
-    printlog("Received '%s' from %s, %d seconds after startup." % (data_str, address, time.time() - DAEMON_START_TIME))
+    printlog("Received data from %s, %d seconds after startup." % (address, time.time() - DAEMON_START_TIME))
     printlog("From socket: %s" % sock)
 
-    data_upper = data_str.upper()
-    printlog("Sending response...")
+    byte_data = bytearray(data)
+
+    request_data = {
+        'version': byte_data[0],
+        'opcode': byte_data[1],
+        'reserved': int.from_bytes(byte_data[2:4], byteorder='big'),
+        'internal_port': int.from_bytes(byte_data[4:6], byteorder='big'),
+        'external_port': int.from_bytes(byte_data[6:8], byteorder='big'),
+        'requested_lifetime': int.from_bytes(byte_data[8:12], byteorder='big'),
+    }
+
+    print("Request data: " + str(request_data))
+
+    response_bytes = bytes()
+
+    response_bytes += (0).to_bytes(1, 'big')
+    response_bytes += (request_data["opcode"] + 128).to_bytes(1, 'big')
+    response_bytes += (0).to_bytes(2, 'big')
+    response_bytes += (int(time.time() - DAEMON_START_TIME)).to_bytes(4, 'big')
+    response_bytes += (request_data["internal_port"]).to_bytes(2, 'big')
+    response_bytes += (request_data["external_port"]).to_bytes(2, 'big')
+    response_bytes += (request_data["requested_lifetime"]).to_bytes(4, 'big')
+
+    print(response_bytes)
+    print(len(response_bytes))
 
     from network_module import send_udp_response
-    send_udp_response(bytearray(data_upper, "utf-8"), address[0], sock)
+    send_udp_response(response_bytes, address[0], sock)
