f61cbed51dbec8641fe824ecea6b0f108ddfc3f0
agubelu <agustinbd3@gmail.com>
2017-04-19 11:44:16 +0200

    Refactor into a more clean structure

--- server_exceptions.py
+++
@@ -1,8 +0,0 @@
-# Must occur when a received UDP packet does not fit the NAT-PMP specification
-class MalformedPacketException(Exception):
-    pass
-
-# Must occur when a desired port is already reserved from other mapping request
-class PortAlreadyReservedException(Exception):
-    pass
-
