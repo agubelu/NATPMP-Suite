29f75ccb0bd35e1188b21c7b1ff2d4ea37cc443d
agubelu <agustinbd3@gmail.com>
2017-04-30 17:42:18 +0200

    More work into the client

--- client/gui/main_frame.py
+++ client/gui/main_frame.py
@@ -1,4 +1,4 @@
-from tkinter import Frame, BOTH, X, Label, Entry, LEFT, RIGHT, E, N, Button, CENTER
+from tkinter import Frame, BOTH, X, Label, Entry, LEFT, RIGHT, E, N, W, Button, CENTER, ttk, Checkbutton, NE, BOTTOM
 
 
 LABEL_WIDTH = 10
@@ -22,6 +22,13 @@ class MainFrame(Frame):
         frame_right = Frame(self)
         frame_right.pack(side=RIGHT, anchor=N)
 
+        frame_send_button = Frame(self)
+        frame_send_button.pack(side=BOTTOM, anchor=N)
+
+        number_validator = self.register(is_number)
+        number_dot_validator = self.register(number_or_dot)
+        number_dot_comma_validator = self.register(number_or_dot_or_comma)
+
         ############################################################################################
 
         frame_ver = Frame(frame_left)
@@ -30,8 +37,10 @@ class MainFrame(Frame):
         self.label_version = Label(frame_ver, text="Version:", width=LABEL_WIDTH, anchor=E)
         self.label_version.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
-        self.entry_version = Entry(frame_ver)
-        self.entry_version.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
+        self.select_version = ttk.Combobox(frame_ver, values=("NAT-PMP v0 (official)", "NAT-PMP v1 (custom)"))
+        self.select_version.set("NAT-PMP v0 (official)")
+        self.select_version.configure(state='readonly')
+        self.select_version.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
 
         ############################################################################################
 
@@ -41,8 +50,10 @@ class MainFrame(Frame):
         self.label_operation = Label(frame_operation, text="Operation:", width=LABEL_WIDTH, anchor=E)
         self.label_operation.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
-        self.entry_operation = Entry(frame_operation)
-        self.entry_operation.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
+        self.select_operation = ttk.Combobox(frame_operation, values=("NAT-PMP discovery", "TCP mapping", "UDP mapping"))
+        self.select_operation.set("NAT-PMP discovery")
+        self.select_operation.configure(state='readonly')
+        self.select_operation.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
 
         ############################################################################################
 
@@ -52,7 +63,7 @@ class MainFrame(Frame):
         self.label_privport = Label(frame_privport, text="Private port:", width=LABEL_WIDTH, anchor=E)
         self.label_privport.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
-        self.entry_privport = Entry(frame_privport)
+        self.entry_privport = Entry(frame_privport, validate='all', validatecommand=(number_validator, '%P'))
         self.entry_privport.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
 
         ############################################################################################
@@ -63,7 +74,7 @@ class MainFrame(Frame):
         self.label_pubport = Label(frame_pubport, text="Private port:", width=LABEL_WIDTH, anchor=E)
         self.label_pubport.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
-        self.entry_pubport = Entry(frame_pubport)
+        self.entry_pubport = Entry(frame_pubport, validate='all', validatecommand=(number_validator, '%P'))
         self.entry_pubport.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
 
         ############################################################################################
@@ -74,7 +85,7 @@ class MainFrame(Frame):
         self.label_lifetime = Label(frame_lifetime, text="Lifetime:", width=LABEL_WIDTH, anchor=E)
         self.label_lifetime.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
-        self.entry_lifetime = Entry(frame_lifetime)
+        self.entry_lifetime = Entry(frame_lifetime, validate='all', validatecommand=(number_validator, '%P'))
         self.entry_lifetime.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
 
         ############################################################################################
@@ -85,7 +96,7 @@ class MainFrame(Frame):
         self.label_ips = Label(frame_ips, text="Public IPs:", width=LABEL_WIDTH, anchor=E)
         self.label_ips.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
-        self.entry_ips = Entry(frame_ips)
+        self.entry_ips = Entry(frame_ips, validate='all', validatecommand=(number_dot_comma_validator, '%P'))
         self.entry_ips.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
 
         ############################################################################################
@@ -96,7 +107,7 @@ class MainFrame(Frame):
         self.label_gateway = Label(frame_gateway, text="Gateway:", width=LABEL_WIDTH, anchor=E)
         self.label_gateway.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
-        self.entry_gateway = Entry(frame_gateway)
+        self.entry_gateway = Entry(frame_gateway, validate='all', validatecommand=(number_dot_validator, '%P'))
         self.entry_gateway.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
 
         ############################################################################################
@@ -107,30 +118,34 @@ class MainFrame(Frame):
         self.label_usetls = Label(frame_usetls, text="Use TLS:", width=LABEL_WIDTH, anchor=E)
         self.label_usetls.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
-        self.entry_usetls = Entry(frame_usetls)
-        self.entry_usetls.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
+        self.check_usetls = Checkbutton(frame_usetls, anchor=W)
+        self.check_usetls.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
 
         ############################################################################################
 
         frame_cert = Frame(frame_right)
         frame_cert.pack(fill=X)
 
-        self.label_cert = Label(frame_cert, text="Client cert:", width=LABEL_WIDTH, anchor=E)
-        self.label_cert.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
+        self.label_cert = Label(frame_cert, text="Client cert:", width=LABEL_WIDTH)
+        self.label_cert.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT, anchor=NE)
 
-        self.entry_cert = Entry(frame_cert)
-        self.entry_cert.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
+        self.button_cert = Button(frame_cert, text="Select")
+        self.button_cert.pack(padx=PADDING_AMOUNT, expand=False, anchor=W)
+        self.text_cert = Label(frame_cert, text="No file selected")
+        self.text_cert.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
         ############################################################################################
 
         frame_key = Frame(frame_right)
         frame_key.pack(fill=X)
 
-        self.label_key = Label(frame_key, text="Client key:", width=LABEL_WIDTH, anchor=E)
-        self.label_key.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
+        self.label_key = Label(frame_key, text="Client key:", width=LABEL_WIDTH)
+        self.label_key.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT, anchor=NE)
 
-        self.entry_key = Entry(frame_key)
-        self.entry_key.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
+        self.button_key = Button(frame_key, text="Select")
+        self.button_key.pack(padx=PADDING_AMOUNT, expand=False, anchor=W)
+        self.text_key = Label(frame_key, text="No file selected")
+        self.text_key.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
         ############################################################################################
 
@@ -142,8 +157,28 @@ class MainFrame(Frame):
 
         ############################################################################################
 
-        frame_button = Frame(frame_right)
+        frame_button = Frame(frame_send_button)
         frame_button.pack(fill=X)
 
-        self.button_send = Button(frame_button, text="Send", anchor=CENTER)
+        self.button_send = Button(frame_send_button, text="Send", anchor=CENTER, height=2, width=10)
         self.button_send.pack(fill=X, padx=PADDING_AMOUNT)
+
+
+############################################################################################
+############################################################################################
+############################################################################################
+
+def is_number(text):
+    try:
+        int(text)
+        return True
+    except ValueError:
+        return False
+
+
+def number_or_dot(text):
+    return all(is_number(x) or x == "." for x in text)
+
+
+def number_or_dot_or_comma(text):
+    return all(is_number(x) or x == "." or x == "," for x in text)
