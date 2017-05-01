8f0baa0b20c61d4b8ebea72f5d7b131d62f39e7b
agubelu <agustinbd3@gmail.com>
2017-05-01 18:10:14 +0200

    Doing GUIs is a pain

--- client/gui/main_frame.py
+++ client/gui/main_frame.py
@@ -1,4 +1,4 @@
-from tkinter import ttk
+from tkinter import ttk, filedialog
 from tkinter import *
 
 
@@ -33,7 +33,7 @@ class MainFrame(Frame):
         frame_send_button.pack(side=BOTTOM, anchor=N)
 
         frame_info_text = Frame(self)
-        frame_info_text.pack(side=BOTTOM, anchor=S)
+        frame_info_text.pack(side=BOTTOM, anchor=S, fill=BOTH, expand=TRUE)
 
         number_validator = self.register(is_number)
         number_dot_validator = self.register(number_or_dot)
@@ -47,7 +47,9 @@ class MainFrame(Frame):
         self.label_version = Label(frame_ver, text="Version:", width=LABEL_WIDTH, anchor=E)
         self.label_version.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
-        self.select_version = ttk.Combobox(frame_ver, values=("NAT-PMP v0 (official)", "NAT-PMP v1 (custom)"))
+        var_select_version = StringVar()
+        self.select_version = ttk.Combobox(frame_ver, values=("NAT-PMP v0 (official)", "NAT-PMP v1 (custom)"), textvariable=var_select_version)
+        self.select_version.var = var_select_version
         self.select_version.set("NAT-PMP v0 (official)")
         self.select_version.configure(state='readonly')
         self.select_version.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
@@ -60,7 +62,9 @@ class MainFrame(Frame):
         self.label_operation = Label(frame_operation, text="Operation:", width=LABEL_WIDTH, anchor=E)
         self.label_operation.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
-        self.select_operation = ttk.Combobox(frame_operation, values=("NAT-PMP discovery", "TCP mapping", "UDP mapping"))
+        var_select_operation = StringVar()
+        self.select_operation = ttk.Combobox(frame_operation, values=("NAT-PMP discovery", "TCP mapping", "UDP mapping"), textvariable=var_select_operation)
+        self.select_operation.var = var_select_operation
         self.select_operation.set("NAT-PMP discovery")
         self.select_operation.configure(state='readonly')
         self.select_operation.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
@@ -73,7 +77,9 @@ class MainFrame(Frame):
         self.label_privport = Label(frame_privport, text="Private port:", width=LABEL_WIDTH, anchor=E)
         self.label_privport.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
-        self.entry_privport = Entry(frame_privport, validate='all', validatecommand=(number_validator, '%P'))
+        var_private_port = StringVar()
+        self.entry_privport = Entry(frame_privport, validate='all', validatecommand=(number_validator, '%P'), textvariable=var_private_port)
+        self.entry_privport.var = var_private_port
         self.entry_privport.configure(state='readonly')
         self.entry_privport.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
 
@@ -85,7 +91,9 @@ class MainFrame(Frame):
         self.label_pubport = Label(frame_pubport, text="Private port:", width=LABEL_WIDTH, anchor=E)
         self.label_pubport.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
-        self.entry_pubport = Entry(frame_pubport, validate='all', validatecommand=(number_validator, '%P'))
+        var_public_port = StringVar()
+        self.entry_pubport = Entry(frame_pubport, validate='all', validatecommand=(number_validator, '%P'), textvariable=var_public_port)
+        self.entry_pubport.var = var_public_port
         self.entry_pubport.configure(state='readonly')
         self.entry_pubport.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
 
@@ -97,7 +105,9 @@ class MainFrame(Frame):
         self.label_lifetime = Label(frame_lifetime, text="Lifetime:", width=LABEL_WIDTH, anchor=E)
         self.label_lifetime.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
-        self.entry_lifetime = Entry(frame_lifetime, validate='all', validatecommand=(number_validator, '%P'))
+        var_lifetime = StringVar()
+        self.entry_lifetime = Entry(frame_lifetime, validate='all', validatecommand=(number_validator, '%P'), textvariable=var_lifetime)
+        self.entry_lifetime.var = var_lifetime
         self.entry_lifetime.configure(state='readonly')
         self.entry_lifetime.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
 
@@ -109,7 +119,9 @@ class MainFrame(Frame):
         self.label_ips = Label(frame_ips, text="Public IPs:", width=LABEL_WIDTH, anchor=E)
         self.label_ips.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
-        self.entry_ips = Entry(frame_ips, validate='all', validatecommand=(number_dot_comma_validator, '%P'))
+        var_ips = StringVar()
+        self.entry_ips = Entry(frame_ips, validate='all', validatecommand=(number_dot_comma_validator, '%P'), textvariable=var_ips)
+        self.entry_ips.var = var_ips
         self.entry_ips.configure(state='readonly')
         self.entry_ips.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
 
@@ -121,7 +133,9 @@ class MainFrame(Frame):
         self.label_gateway = Label(frame_gateway, text="Gateway:", width=LABEL_WIDTH, anchor=E)
         self.label_gateway.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
-        self.entry_gateway = Entry(frame_gateway, validate='all', validatecommand=(number_dot_validator, '%P'))
+        var_gateway = StringVar(value="default")
+        self.entry_gateway = Entry(frame_gateway, validate='all', validatecommand=(number_dot_validator, '%P'), textvariable=var_gateway, fg="#666666")
+        self.entry_gateway.var = var_gateway
         self.entry_gateway.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
 
         ############################################################################################
@@ -134,6 +148,7 @@ class MainFrame(Frame):
 
         var_check_tls = IntVar()
         self.check_usetls = Checkbutton(frame_usetls, variable=var_check_tls, anchor=W)
+        self.check_usetls.configure(state='disabled')
         self.check_usetls.var = var_check_tls
         self.check_usetls.pack(fill=X, padx=PADDING_AMOUNT, expand=True, anchor=W)
 
@@ -148,7 +163,7 @@ class MainFrame(Frame):
         self.button_cert = Button(frame_cert, text="Select")
         self.button_cert.configure(state='disabled')
         self.button_cert.pack(padx=PADDING_AMOUNT, expand=False, anchor=W)
-        self.text_cert = Label(frame_cert, text="No file selected")
+        self.text_cert = Label(frame_cert, text="No file selected", justify=LEFT)
         self.text_cert.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
         ############################################################################################
@@ -162,7 +177,7 @@ class MainFrame(Frame):
         self.button_key = Button(frame_key, text="Select")
         self.button_key.configure(state='disabled')
         self.button_key.pack(padx=PADDING_AMOUNT, expand=False, anchor=W)
-        self.text_key = Label(frame_key, text="No file selected")
+        self.text_key = Label(frame_key, text="No file selected", justify=LEFT)
         self.text_key.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
         ############################################################################################
@@ -184,7 +199,7 @@ class MainFrame(Frame):
         ############################################################################################
 
         frame_text = Frame(frame_info_text)
-        frame_text.pack(fill=X)
+        frame_text.pack(fill=BOTH, expand=TRUE)
 
         scrollbar = ttk.Scrollbar(frame_text, orient=VERTICAL)
 
@@ -195,7 +210,7 @@ class MainFrame(Frame):
         scrollbar.pack(fill=Y, side=RIGHT)
 
         self.info_text.configure(state=DISABLED)
-        self.info_text.pack(fill=X, padx=PADDING_AMOUNT)
+        self.info_text.pack(fill=BOTH, padx=PADDING_AMOUNT, expand=TRUE)
 
     def init_handlers(self):
         # Initialise the various handlers
@@ -210,6 +225,82 @@ class MainFrame(Frame):
                 self.button_key.configure(state='disabled')
 
         self.check_usetls.configure(command=usetls_check_handler)
+
+        # Handler for the version and operation selectors, enables or disables the corresponding fields
+        def version_operation_handler(_):
+            v0 = self.select_version.var.get() == "NAT-PMP v0 (official)"
+            info = self.select_operation.var.get() == "NAT-PMP discovery"
+
+            if v0:
+                self.check_usetls.var.set(0)
+                self.check_usetls.configure(state="disabled")
+                self.button_cert.configure(state='disabled')
+                self.button_key.configure(state='disabled')
+            else:
+                self.check_usetls.configure(state="normal")
+                self.button_cert.configure(state='normal')
+                self.button_key.configure(state='normal')
+
+            if info:
+                # Disable all entries, no matter the version
+                self.entry_privport.configure(state="readonly")
+                self.entry_pubport.configure(state="readonly")
+                self.entry_lifetime.configure(state="readonly")
+                self.entry_ips.configure(state="readonly")
+            elif v0:
+                # Request in v0, enable all but the public IPS field
+                self.entry_privport.configure(state="normal")
+                self.entry_pubport.configure(state="normal")
+                self.entry_lifetime.configure(state="normal")
+                self.entry_ips.configure(state="readonly")
+            else:
+                # Request in v1, enable all
+                self.entry_privport.configure(state="normal")
+                self.entry_pubport.configure(state="normal")
+                self.entry_lifetime.configure(state="normal")
+                self.entry_ips.configure(state="normal")
+
+        self.select_version.bind("<<ComboboxSelected>>", version_operation_handler)
+        self.select_operation.bind("<<ComboboxSelected>>", version_operation_handler)
+
+        # Handlers for the placeholder text in the gateway entry
+        def gateway_focusin_handler(_):
+            if self.entry_gateway.var.get() == "default":
+                self.entry_gateway.configure(fg="#000000")
+                self.entry_gateway.var.set("")
+
+        def gateway_focusout_handler(_):
+            if self.entry_gateway.var.get() == "":
+                self.entry_gateway.configure(fg="#666666")
+                self.entry_gateway.var.set("default")
+
+        self.entry_gateway.bind("<FocusIn>", gateway_focusin_handler)
+        self.entry_gateway.bind("<FocusOut>", gateway_focusout_handler)
+
+        # Handler for selecting files (client cert/key)
+        def select_file_handler(button):
+            file = filedialog.askopenfilename()
+            text = ""
+            n = 0
+
+            for c in file:
+                text += c
+                n += 1
+                if n >= 20 and (c == "/" or c == "\\"):
+                    text += "\n"
+                    n = 0
+
+            if not text:
+                text = "No file selected"
+
+            if button == "cert":
+                self.text_cert.configure(text=text)
+            else:
+                self.text_key.configure(text=text)
+
+        self.button_cert.configure(command=lambda: select_file_handler("cert"))
+        self.button_key.configure(command=lambda: select_file_handler("key"))
+
 
 ############################################################################################
 ############################################################################################
