147176bb5036c058eb10c5812e5dad4342e6d123
agubelu <agustinbd3@gmail.com>
2017-05-01 17:02:07 +0200

    More work into the client GUI...

--- client/gui/main_frame.py
+++ client/gui/main_frame.py
@@ -1,4 +1,5 @@
-from tkinter import Frame, BOTH, X, Label, Entry, LEFT, RIGHT, E, N, W, Button, CENTER, ttk, Checkbutton, NE, BOTTOM
+from tkinter import ttk
+from tkinter import *
 
 
 LABEL_WIDTH = 10
@@ -11,19 +12,28 @@ class MainFrame(Frame):
         Frame.__init__(self, parent)
         self.parent = parent
         self.init_gui()
+        self.init_handlers()
 
     def init_gui(self):
+        # Initialise the user interface layout
+
         self.parent.title("NAT-PMP Client")
         self.pack(fill=BOTH, expand=True)
 
-        frame_left = Frame(self)
+        frame_top_controls = Frame(self)
+        frame_top_controls.pack(side=TOP, anchor=N)
+
+        frame_left = Frame(frame_top_controls)
         frame_left.pack(side=LEFT, anchor=N)
 
-        frame_right = Frame(self)
+        frame_right = Frame(frame_top_controls)
         frame_right.pack(side=RIGHT, anchor=N)
 
         frame_send_button = Frame(self)
         frame_send_button.pack(side=BOTTOM, anchor=N)
+
+        frame_info_text = Frame(self)
+        frame_info_text.pack(side=BOTTOM, anchor=S)
 
         number_validator = self.register(is_number)
         number_dot_validator = self.register(number_or_dot)
@@ -64,6 +74,7 @@ class MainFrame(Frame):
         self.label_privport.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
         self.entry_privport = Entry(frame_privport, validate='all', validatecommand=(number_validator, '%P'))
+        self.entry_privport.configure(state='readonly')
         self.entry_privport.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
 
         ############################################################################################
@@ -75,6 +86,7 @@ class MainFrame(Frame):
         self.label_pubport.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
         self.entry_pubport = Entry(frame_pubport, validate='all', validatecommand=(number_validator, '%P'))
+        self.entry_pubport.configure(state='readonly')
         self.entry_pubport.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
 
         ############################################################################################
@@ -86,6 +98,7 @@ class MainFrame(Frame):
         self.label_lifetime.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
         self.entry_lifetime = Entry(frame_lifetime, validate='all', validatecommand=(number_validator, '%P'))
+        self.entry_lifetime.configure(state='readonly')
         self.entry_lifetime.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
 
         ############################################################################################
@@ -97,6 +110,7 @@ class MainFrame(Frame):
         self.label_ips.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
         self.entry_ips = Entry(frame_ips, validate='all', validatecommand=(number_dot_comma_validator, '%P'))
+        self.entry_ips.configure(state='readonly')
         self.entry_ips.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
 
         ############################################################################################
@@ -118,8 +132,10 @@ class MainFrame(Frame):
         self.label_usetls = Label(frame_usetls, text="Use TLS:", width=LABEL_WIDTH, anchor=E)
         self.label_usetls.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
 
-        self.check_usetls = Checkbutton(frame_usetls, anchor=W)
-        self.check_usetls.pack(fill=X, padx=PADDING_AMOUNT, expand=True)
+        var_check_tls = IntVar()
+        self.check_usetls = Checkbutton(frame_usetls, variable=var_check_tls, anchor=W)
+        self.check_usetls.var = var_check_tls
+        self.check_usetls.pack(fill=X, padx=PADDING_AMOUNT, expand=True, anchor=W)
 
         ############################################################################################
 
@@ -130,6 +146,7 @@ class MainFrame(Frame):
         self.label_cert.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT, anchor=NE)
 
         self.button_cert = Button(frame_cert, text="Select")
+        self.button_cert.configure(state='disabled')
         self.button_cert.pack(padx=PADDING_AMOUNT, expand=False, anchor=W)
         self.text_cert = Label(frame_cert, text="No file selected")
         self.text_cert.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
@@ -143,6 +160,7 @@ class MainFrame(Frame):
         self.label_key.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT, anchor=NE)
 
         self.button_key = Button(frame_key, text="Select")
+        self.button_key.configure(state='disabled')
         self.button_key.pack(padx=PADDING_AMOUNT, expand=False, anchor=W)
         self.text_key = Label(frame_key, text="No file selected")
         self.text_key.pack(side=LEFT, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT)
@@ -163,10 +181,55 @@ class MainFrame(Frame):
         self.button_send = Button(frame_send_button, text="Send", anchor=CENTER, height=2, width=10)
         self.button_send.pack(fill=X, padx=PADDING_AMOUNT)
 
+        ############################################################################################
+
+        frame_text = Frame(frame_info_text)
+        frame_text.pack(fill=X)
+
+        scrollbar = ttk.Scrollbar(frame_text, orient=VERTICAL)
+
+        self.info_text = Text(frame_text, height=10, padx=PADDING_AMOUNT, pady=PADDING_AMOUNT, yscrollcommand=scrollbar.set)
+        self.info_text.insert(END, "Status: idle")
+
+        scrollbar.config(command=self.info_text.yview)
+        scrollbar.pack(fill=Y, side=RIGHT)
+
+        self.info_text.configure(state=DISABLED)
+        self.info_text.pack(fill=X, padx=PADDING_AMOUNT)
+
+    def init_handlers(self):
+        # Initialise the various handlers
+
+        # Handler for the "Use TLS" button, enables or disables the cert and key buttons
+        def usetls_check_handler():
+            if self.check_usetls.var.get():
+                self.button_cert.configure(state='normal')
+                self.button_key.configure(state='normal')
+            else:
+                self.button_cert.configure(state='disabled')
+                self.button_key.configure(state='disabled')
+
+        self.check_usetls.configure(command=usetls_check_handler)
 
 ############################################################################################
 ############################################################################################
 ############################################################################################
+
+    def reset_info_text(self):
+        self.info_text.configure(state=NORMAL)
+        self.info_text.delete(1.0, END)
+        self.info_text.configure(state=DISABLED)
+
+    def insert_info_line(self, text):
+        self.info_text.configure(state=NORMAL)
+        self.info_text.insert(END, text)
+        self.info_text.see(END)
+        self.info_text.configure(state=DISABLED)
+
+############################################################################################
+############################################################################################
+############################################################################################
+
 
 def is_number(text):
     try:
