75db201c6c834a08ce5bfef9bbadf7b6fe607591
agubelu <agustinbd3@gmail.com>
2017-05-10 20:39:28 +0200

    Working on the templates

--- natpmp_operation/web_interface_module.py
+++ natpmp_operation/web_interface_module.py
@@ -1,4 +1,4 @@
-from flask                          import Flask, session, send_from_directory
+from flask                          import Flask, session, request, render_template
 
 from natpmp_operation.common_utils  import printlog
 
@@ -8,16 +8,18 @@ import os
 
 def init_web_interface():
 
+    # TODO quitar el logging de flask
+    # TODO hacer from natpmp_operation import natpmp_logic_common dentro del metodo
     flask_app = Flask(__name__, static_folder="webapp/static", template_folder="webapp/templates")
     flask_app.secret_key = os.urandom(16)
     interface_port = settings.WEB_INTERFACE_PORT
 
     # Create the routers
 
-    @flask_app.route("/")
+    @flask_app.route("/", methods=["GET", "POST"])
     def hello_world():
-        from natpmp_operation import natpmp_logic_common
-        return str(natpmp_logic_common.CURRENT_MAPPINGS)
+        if request.method == "GET":
+            return render_template("index.html", message=None)
 
     # Run the app
     printlog("Web interface up and running at port %s" % interface_port)
